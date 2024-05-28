22782317c121633ae1ab949e5f8f585f1e5c8785
uesyn <suemura@zlab.co.jp>
2024-05-28 13:04:09 +0900

    Fix keymap

--- nvim/lua/user/utils.lua
+++ nvim/lua/user/utils.lua
@@ -16,6 +16,7 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Set up fzf-lua {{
 vim.keymap.set("n", "<Leader>fs", "<Cmd>lua require('fzf-lua').live_grep()<CR>")
 vim.keymap.set("n", "<Leader>ff", "<Cmd>lua require('fzf-lua').files()<CR>")
+vim.keymap.set("n", "<Leader>fb", "<Cmd>lua require('fzf-lua').blines()<CR>")
 -- }}}
 
 -- Set up openingh {{

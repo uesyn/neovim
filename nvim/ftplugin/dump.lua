2bb9efc92b906487a386cfd34c76c94ff51866e3
uesyn <17411645+uesyn@users.noreply.github.com>
2024-06-22 18:36:15 +0000

    feat(barbar.nvim): Auto hide tabline and disable in dump filetype
    This commit makes two changes related to the barbar.nvim plugin. First,
    it enables the auto_hide feature, which automatically hides the tabline
    when there's only one buffer open. This is done by adding the
    "auto_hide" option to the barbar setup call.
    
    Second, it disables the barbar plugin entirely when the filetype is
    "dump". This is done by removing the BarbarDisable command and the
    showtabline option setting from the dump filetype plugin script.

--- nvim/ftplugin/dump.lua
+++ nvim/ftplugin/dump.lua
@@ -1,5 +1,3 @@
 vim.opt.number = false
 vim.opt.relativenumber = false
 vim.opt.signcolumn = "no"
-vim.cmd[[BarbarDisable]]
-vim.opt.showtabline = 0
--- nvim/lua/user/plugins/barbar_nvim.lua
+++ nvim/lua/user/plugins/barbar_nvim.lua
@@ -1,7 +1,10 @@
 return {
   "barbar.nvim",
   after = function()
-    require("barbar").setup({ ["animation"] = false })
+    require("barbar").setup({
+      ["animation"] = false,
+      ["auto_hide"] = 1,
+    })
     vim.keymap.set("n", "<C-n>", "<Cmd>BufferNext<CR>")
     vim.keymap.set("n", "<C-p>", "<Cmd>BufferPrevious<CR>")
     vim.keymap.set("n", "<C-q>", "<Cmd>BufferClose<CR>")

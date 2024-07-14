0fc6625de2c383eee6d4d44194de8f2044ca8882
uesyn <17411645+uesyn@users.noreply.github.com>
2024-07-14 05:59:05 +0000

    fixup! fixup! feat(neovim): Update plugins and configurations

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -159,6 +159,12 @@ return {
       { name = "cmp_snippy", dir = "@cmp_snippy@" },
     },
     config = function()
+      local has_words_before = function()
+        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+      end
+
       local cmp = require("cmp")
       cmp.setup({
         snippet = {
@@ -176,7 +182,13 @@ return {
           ["<C-x><C-o>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = false }),
-          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
+            end
+          end),
         }),
         sources = {
           { name = "nvim_lsp" },
